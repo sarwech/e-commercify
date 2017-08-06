5bb71dc11af45794bedfb07e07403d691d020080
Sarwech Shar <sarwech.shar@gmail.com>
2017-08-06 23:44:32 +0100

    select men/women to filter items

--- src/App.js
+++ src/App.js
@@ -5,40 +5,51 @@ import './App.css';
 
 const IMAGES =
 [{
-        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/2499011/333894655_rjb0EWeKwY/P1.jpg",
+        _id: 1,
+        src: "https://www.depop.com/georgebarry/georgebarry-vintage-tommy-hilfiger-panel-logo-fleece-2",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/2499011/333894655_rjb0EWeKwY/P1.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 320,
         isSelected: false,
         tags: [{value: "£80", title: "£80"}],
-        caption: "Vintage Tommy Hilfiger panel-logo fleece."
+        caption: "Vintage Tommy Hilfiger panel-logo fleece.",
+        value: "Vintage Tommy Hilfiger panel-logo fleece.",
+        gender: "Men"
 },
 {
+        _id: 2,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 320,
         tags: [{value: "£20", title: "£20"}],
-        caption: "Vintage Retro Kappa LUFC Jacket"
+        caption: "Vintage Retro Kappa LUFC Jacket",
+        value: "Vintage Retro Kappa LUFC Jacket",
+        gender: "Women"
+
 },
  
 {
+        _id: 3,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/5117243/333894886_TDyz0aFVUt/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/5117243/333894886_TDyz0aFVUt/P1.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 320,
         tags: [{value: "£25", title: "£25"}],
-        caption: "Gorgeous Zara trafuluc distressed denim pinafore dress with raw hem and blue detailing."
+        caption: "Gorgeous Zara trafuluc distressed denim pinafore dress with raw hem and blue detailing.",
+        gender: "Women"
 },
 {
+        _id: 4,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/2499011/333891910_sMZx5p4tX6/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/2499011/333891910_sMZx5p4tX6/P1.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 320,
         tags: [{value: "£25", title: "£25"}],
-        caption: "Vintage Ellesse fleece."
+        caption: "Vintage Ellesse fleece.",
 },
 {
+        _id: 5,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/2499011/333893448_80i1GZhmJN/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/2499011/333893448_80i1GZhmJN/P1.jpg",
         thumbnailWidth: 320,
@@ -48,22 +59,27 @@ const IMAGES =
 },
  
 {
+        _id: 6,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/3028144/333878474_1r69cP5QIk/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/3028144/333878474_1r69cP5QIk/P1.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 320,
         tags: [{value: "£28", title: "£28"}],
-        caption: "orange 🍊 suede slip on midi high heels ? shoes."
+        caption: "orange 🍊 suede slip on midi high heels ? shoes.",
+        gender: "Women"
 },
 {
+        _id: 7,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/3920651/333880601_6H1tTPdEzj/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/3920651/333880601_6H1tTPdEzj/P1.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 320,
         tags: [{value: "$34", title: "$34"}],
-        caption: "Such a great pair of GUESS jeans."
+        caption: "Such a great pair of GUESS jeans.",
+        gender: "Women"
 },
 {
+        _id: 8,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/3296009/333880913_J49yaUYzml/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/3296009/333880913_J49yaUYzml/P1.jpg",
         thumbnailWidth: 320,
@@ -73,6 +89,7 @@ const IMAGES =
 },
  
 {
+        _id: 9,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/1639128/333881984_Jc3xe4EBT1/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/1639128/333881984_Jc3xe4EBT1/P1.jpg",
         thumbnailWidth: 320,
@@ -81,6 +98,7 @@ const IMAGES =
         caption: "Lovely VINTAGE beige floral and square patterned button up blouse. ."
 },
 {
+        _id: 10,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnailWidth: 320,
@@ -89,31 +107,7 @@ const IMAGES =
         caption: "Vintage Retro Kappa LUFC Jacket"
 },
 {
-        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
-        thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 320,
-        tags: [{value: "£20", title: "£20"}],
-        caption: "Vintage Retro Kappa LUFC Jacket"
-},
- 
-{
-        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
-        thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 320,
-        tags: [{value: "£20", title: "£20"}],
-        caption: "Vintage Retro Kappa LUFC Jacket"
-},
-{
-        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
-        thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
-        thumbnailWidth: 320,
-        thumbnailHeight: 320,
-        tags: [{value: "£20", title: "£20"}],
-        caption: "Vintage Retro Kappa LUFC Jacket"
-},
-{
+        _id: 11,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnailWidth: 320,
@@ -123,6 +117,35 @@ const IMAGES =
 },
  
 {
+        _id: 12,
+        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
+        thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 320,
+        tags: [{value: "£20", title: "£20"}],
+        caption: "Vintage Retro Kappa LUFC Jacket"
+},
+{
+        _id: 13,
+        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
+        thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 320,
+        tags: [{value: "£20", title: "£20"}],
+        caption: "Vintage Retro Kappa LUFC Jacket"
+},
+{
+        _id: 14,
+        src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
+        thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 320,
+        tags: [{value: "£20", title: "£20"}],
+        caption: "Vintage Retro Kappa LUFC Jacket"
+},
+ 
+{
+        _id: 15,
         src: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnail: "https://d2h1pu99sxkfvn.cloudfront.net/b0/6312186/333894739_frHoVhv1ew/P1.jpg",
         thumbnailWidth: 320,
@@ -140,10 +163,26 @@ class App extends Component {
 		}
 	}
 
+	_handleClickMen() {
+		this.setState((prevState, props) => {
+			return { images: this.state.images.filter(function(image) {
+				return image.gender === "Men"
+			})}
+		})
+	}
+
+	_handleClickWomen() {
+		this.setState({
+			images: this.state.images.filter(function(image) {
+				return image.gender === "Women"
+			})
+		})
+	}
+
 	render() {
 		return (
 			<div>
-				<Header />
+				<Header images={this.state.images} handleClickMen={() => this._handleClickMen()} handleClickWomen={() => this._handleClickWomen()} />
 				<GalleryGrid images={this.state.images}/>
 			</div>
 			)
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,59 +2,58 @@ import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl, Button, Row, Col } from 'react-bootstrap';
 import '../App.css';
 
-const navBarInstance = (
-	<div>
-		<Navbar inverse className="mainNav">
-	    <Row className="show-grid">
-	      <Col xs={8} md={8}>
-				  <Navbar.Header>
-				    <Navbar.Brand>
-				      <a style={{color: 'white'}} href="#">fashion</a>
-				    </Navbar.Brand>
-				  </Navbar.Header>
-	      </Col>
-	    	<Nav pullRight>
-	      	<Col xs={4} md={4}>  
-			      <FormGroup id="searchBox">
-			      	<input type="search" placeholder="Search" />
-			      </FormGroup>
-	      	</Col>
-	    	</Nav>
-	    </Row>	
-		</Navbar>
-		<Navbar className="itemsNav" inverse collapseOnSelect>
-		  <Navbar.Header>
-		    <Navbar.Toggle />
-		  </Navbar.Header>
-		  <Navbar.Collapse>
-		    <Nav>
-		      <NavDropdown eventKey={3} title="MEN" id="basic-nav-dropdown1">
-		        <MenuItem eventKey={3.1}>Tops</MenuItem>
-		        <MenuItem eventKey={3.2}>Bottoms</MenuItem>
-		        <MenuItem eventKey={3.3}>Shoes</MenuItem>
-		      </NavDropdown>
-		      <NavDropdown eventKey={4} title="WOMEN" id="basic-nav-dropdown2">
-		        <MenuItem eventKey={4.1}>Tops</MenuItem>
-		        <MenuItem eventKey={4.2}>Bottoms</MenuItem>
-		        <MenuItem eventKey={4.3}>Shoes</MenuItem>
-		      </NavDropdown>
-		      <NavItem eventKey={5} href="#" id="basic-nav">ACCESSORIES</NavItem>
-		    </Nav>
-		    <Nav pullRight>
-			    <NavItem eventKey={3} href="#">MY ACCOUNT</NavItem>
-		    </Nav>
-		  </Navbar.Collapse>
-		</Navbar>
-	</div>
-);
-
 class Header extends Component {
+	constructor(props) {
+		super(props);
+	}
 
 	render() {
 		return (
 	    <div className="App">
 	      <div className="App-header">
-	      	{navBarInstance}
+			<div>
+				<Navbar inverse className="mainNav">
+			    <Row className="show-grid">
+			      <Col xs={8} md={8}>
+						  <Navbar.Header>
+						    <Navbar.Brand>
+						      <a style={{color: 'black'}} href="." >depop</a>
+						    </Navbar.Brand>
+						  </Navbar.Header>
+			      </Col>
+			    	<Nav pullRight>
+			      	<Col xs={4} md={4}>  
+					      <FormGroup id="searchBox">
+					      	<input items={this.props.images} type="search" placeholder="Search" />
+					      </FormGroup>
+			      	</Col>
+			    	</Nav>
+			    </Row>	
+				</Navbar>
+				<Navbar className="itemsNav" inverse collapseOnSelect>
+				  <Navbar.Header>
+				    <Navbar.Toggle />
+				  </Navbar.Header>
+				  <Navbar.Collapse>
+				    <Nav>
+				      <NavDropdown onClick={this.props.handleClickMen} eventKey={3} title="MEN" id="basic-nav-dropdown1">
+				        <MenuItem eventKey={3.1}>Tops</MenuItem>
+				        <MenuItem eventKey={3.2}>Bottoms</MenuItem>
+				        <MenuItem eventKey={3.3}>Shoes</MenuItem>
+				      </NavDropdown>
+				      <NavDropdown onClick={this.props.handleClickWomen} eventKey={4} title="WOMEN" id="basic-nav-dropdown2">
+				        <MenuItem eventKey={4.1}>Tops</MenuItem>
+				        <MenuItem eventKey={4.2}>Bottoms</MenuItem>
+				        <MenuItem eventKey={4.3}>Shoes</MenuItem>
+				      </NavDropdown>
+				      <NavItem eventKey={5} href="#" id="basic-nav">ACCESSORIES</NavItem>
+				    </Nav>
+				    <Nav pullRight>
+					    <NavItem eventKey={3} href="#">MY ACCOUNT</NavItem>
+				    </Nav>
+				  </Navbar.Collapse>
+				</Navbar>
+			</div>
 	      </div>
 	      <p className="App-intro">
 	      </p>
